392706157d13e6960cce709c6a654dd9504c3249
Julio <JulioToboso@gmail.com>
2019-11-04 18:31:56 +0100

    Create Problem.m

---
+++ postes/Problem.m
@@ -0,0 +1,11 @@
+% Tipo de Poste  Pequeño      Mediano         Grande
+% Recursos        
+% Acero (ton.)    1.5             3               5
+% Manoobra (h.)   30              25              40
+% Ganancias €      2000           3000            4000
+% 
+% dispone de 
+% 6000 ton acero
+% 60.000 h de trabajo
+% 
+% al menos 1000 postes por tipo
