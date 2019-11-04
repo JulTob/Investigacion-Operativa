383f9990a801a89eeb2c6e2298047e528724f733
Julio <JulioToboso@gmail.com>
2019-11-04 14:37:00 +0100

    Create Description.m

---
+++ Northern/Description.m
@@ -0,0 +1,14 @@
+%Northen Airplanes Co.
+
+%Planificación de la produccion
+% 
+% 
+% Mes  Instalaciones PMax CostUnitProd  CostUnitAlmaz
+% 
+% 1       10          25      1.08        0.015
+% 2       15          35      1.11        0.015
+% 3       25          30      1.10        0.015
+% 4       20          10      1.13        0.015
+% 
+% 
+% Demanda 10 15 25 20
